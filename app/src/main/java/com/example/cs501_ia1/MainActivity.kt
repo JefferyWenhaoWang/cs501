dd88aaf78b042cb523e900530465d91d4cdf6b1d
Jefferywan <114625429+Jefferywan@users.noreply.github.com>
2026-01-26 20:27:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/cs501_ia1/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.cs501_ia1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cs501_ia1.ui.theme.CS501_IA1Theme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+import androidx.compose.ui.unit.dp
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CS501_IA1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    FirstAppScreen(modifier = Modifier.padding(innerPadding))
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CS501_IA1Theme {
+        FirstAppScreen()
+    }
+}
+
+
+@Composable
+fun FirstAppScreen(modifier: Modifier = Modifier) {
+    var message by remember { mutableStateOf("Hi, I'm Wenhao. This is my CS501 IA1 app.") }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = message, style = MaterialTheme.typography.titleLarge)
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { message = "Button clicked, text updated!" }) {
+            Text("Update text")
+        }
+    }
+}
+

e176569e1398f3bcd789186a0936c5786ceb1c7a
Jefferywan <114625429+Jefferywan@users.noreply.github.com>
2026-02-23 01:01:27 -0500

    Add IA3 Q3 tag browser with FlowRow and FlowColumn

--- app/src/main/java/com/example/cs501_ia1/MainActivity.kt
+++ app/src/main/java/com/example/cs501_ia1/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CS501_IA1Theme {
-                Q2ProfileScreen()
+                Q3TagBrowserScreen()
             }
         }
     }
